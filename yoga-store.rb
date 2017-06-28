054b8428a0ea73f63d3fdc68d69d57e6c3752dbf
wmwaqas <wmwaqasmahmood@hotmail.com>
2017-06-28 16:36:53 +0200

    Method to Display Items

--- yoga-store.rb
+++ yoga-store.rb
@@ -1,1 +1,39 @@
+# Helpers, might come handy
+def print_divider
+  puts "*" * 40
+  puts "\n"
+end
+
+def print_progress_bar
+  3.times { sleep 0.5; print "." }
+  puts "\n"
+end
+
+# Empty Shopping cart
 shopping_cart = []
+
+# Defining Products
+@products = [
+  { reference_number: 1231, name: "Super Lite Mat", price: 10 },
+  { reference_number: 1232, name: "Power Mat", price: 20 },
+  { reference_number: 1233, name: "Block", price: 30 },
+  { reference_number: 1234, name: "Meditation cushion", price: 30 },
+  { reference_number: 1235, name: "The best T-shirt", price: 200 },
+  { reference_number: 1236, name: "The cutest yoga pants", price: 300 },
+  { reference_number: 1237, name: "Bring Yoga To Life", price: 30 },
+  { reference_number: 1238, name: "Light On Yoga", price: 10 }
+]
+
+# Mehtod to display items
+def display_products
+  print_divider
+
+  puts "Welcome to our store! We have following products in our store:"
+  @products.each do |product|
+    puts "Reference Number: #{product[:reference_number]}"
+    puts "Product: #{product[:name]}"
+    puts "Price: #{product[:price]}"
+  end
+end
+
+display_products
